c2364848837d2ba36281713269a51aa45fd638e4
Marcin Gomulka <magomulka@gmail.com>
2022-12-01 08:25:12 +0100

    solution day01

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+lines = []
+for line in sys.stdin:
+    lines.append(line)
+
+elve = []
+elves = []
+for line in lines:
+    if line == '\n':
+        elves.append(sum(elve))
+        elve = []
+    else:
+        elve.append(int(line))
+
+elves.sort(reverse=True)
+print(elves)
+print("part1: ", elves[0])
+print("part2: ", elves[0] + elves[1] + elves[2])
+
