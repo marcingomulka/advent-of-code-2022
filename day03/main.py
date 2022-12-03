70404f4bac6e9f0d513fbf931f1bb250bede02bc
Marcin Gomulka <magomulka@gmail.com>
2022-12-03 07:32:50 +0100

    refactor

--- day03/main.py
+++ day03/main.py
@@ -3,9 +3,9 @@ import sys
 
 def letter_value(letter):
     val = 0
-    if ord('z') >= ord(letter) >= ord('a'):
+    if 'z' >= letter >= 'a':
         val = ord(letter) - ord('a') + 1
-    elif ord('Z') >= ord(letter) >= ord('A'):
+    elif 'Z' >= letter >= 'A':
         val = ord(letter) - ord('A') + 27
     return val
 
@@ -20,15 +20,15 @@ def group_intersection(group):
 
 lines = []
 for line in sys.stdin:
-    lines.append(line)
+    lines.append(line.strip())
 
 group = []
 p1_sum = 0
 p2_sum = 0
 for line in lines:
     line_size = len(line)
-    first = {*line[0:line_size // 2]}
-    second = {*line[line_size//2:]}
+    first = {*line[:line_size // 2]}
+    second = {*line[line_size // 2:]}
 
     common = first.intersection(second)
     if len(common) != 1:
@@ -36,13 +36,12 @@ for line in lines:
         exit(-1)
     p1_sum += letter_value(common.pop())
 
-    line_letters = [*line.strip()]
     if len(group) < 3:
-        group.append(set(line_letters))
+        group.append({*line})
     else:
         group_common = group_intersection(group)
         p2_sum += letter_value(group_common.pop())
-        group = [set(line_letters)]
+        group = [{*line}]
 
 group.append({*lines[-1]})
 group_common = group_intersection(group)
