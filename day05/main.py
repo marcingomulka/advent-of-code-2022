1974569e22da648dae2ef375049cfbe330142a78
Marcin Gomulka <magomulka@gmail.com>
2022-12-05 10:22:53 +0100

    refactor

--- day05/main.py
+++ day05/main.py
@@ -2,19 +2,26 @@ import sys
 from collections import deque
 
 
-def move_crate(stacks, count, start, target):
-    for x in range(count):
-        c = stacks[start-1].pop()
-        stacks[target-1].append(c)
+class Move:
+    def __init__(self, count, start_position, target_position):
+        self.count = count
+        self.start_position = start_position
+        self.target_position = target_position
 
 
-def move_crate_p2(stacks, count, start, target):
+def move_crate_p1(stacks, move):
+    for x in range(move.count):
+        c = stacks[move.start_position - 1].pop()
+        stacks[move.target_position - 1].append(c)
+
+
+def move_crate_p2(stacks, move):
     list = []
-    for x in range(0, count):
-        list.append(stacks[start-1].pop())
+    for x in range(0, move.count):
+        list.append(stacks[move.start_position - 1].pop())
     list.reverse()
     for l in list:
-        stacks[target-1].append(l)
+        stacks[move.target_position - 1].append(l)
 
 
 lines = []
@@ -23,11 +30,7 @@ for line in sys.stdin:
 
 row = []
 rows = []
-
-move_counts = []
-start_pos = []
-target_pos = []
-
+moves = []
 for line in lines:
     if not line.startswith("move"):
         iter = 1
@@ -38,42 +41,31 @@ for line in lines:
         row = []
     else:
         chunks = line.split()
-        move_counts.append(int(chunks[1]))
-        start_pos.append(int(chunks[3]))
-        target_pos.append(int(chunks[5]))
-
-
+        moves.append(Move(int(chunks[1]), int(chunks[3]), int(chunks[5])))
 rows.reverse()
-stacks = []
+stacks_p1 = []
 stacks_p2 = []
 stacks_num = len(rows[1])
 for i in range(stacks_num):
-    stacks.append(deque())
+    stacks_p1.append(deque())
     stacks_p2.append(deque())
-
 for row in rows:
     if len(row) > 0 and row[0] != '1':
         index = 0
         for char in row:
             if char != ' ':
-                stacks[index].append(row[index])
+                stacks_p1[index].append(row[index])
                 stacks_p2[index].append(row[index])
             index += 1
-
-index = 0
-for count in move_counts:
-    start = start_pos[index]
-    target = target_pos[index]
-    move_crate(stacks, count, start, target)
-    move_crate_p2(stacks_p2, count, start, target)
-    index += 1
+for move in moves:
+    move_crate_p1(stacks_p1, move)
+    move_crate_p2(stacks_p2, move)
 
 result = ""
-for s in stacks:
+for s in stacks_p1:
     result += s.pop()
 result2 = ""
 for s in stacks_p2:
     result2 += s.pop()
-
 print("part1: ", result)
 print("part2: ", result2)
