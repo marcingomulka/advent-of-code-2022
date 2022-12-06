5094364f01e3010d837d86bb8b711097e99fce5f
Marcin Gomulka <magomulka@gmail.com>
2022-12-06 07:37:46 +0100

    small refactor

--- day06/main.py
+++ day06/main.py
@@ -5,27 +5,30 @@ lines = []
 for line in sys.stdin:
     lines.append(line)
 
-char_list = [*lines[0]]
+char_list = lines[0]
 buffer_p1 = deque()
-buffer_p2 = deque()
-
 p1_result = 0
-p2_result = 0
 for i in range(len(char_list)):
     char = char_list[i]
-    if p1_result == 0 and len(buffer_p1) == 4 and len(set(buffer_p1)) == 4:
+    if len(buffer_p1) == 4 and len(set(buffer_p1)) == 4:
         p1_result = i
+        break
     else:
         if len(buffer_p1) == 4:
             buffer_p1.popleft()
         buffer_p1.append(char)
 
-    if p2_result == 0 and (len(buffer_p2)) == 14 and len(set(buffer_p2)) == 14:
+buffer_p2 = deque()
+p2_result = 0
+for i in range(len(char_list)):
+    char = char_list[i]
+    if len(buffer_p2) == 14 and len(set(buffer_p2)) == 14:
         p2_result = i
+        break
     else:
         if len(buffer_p2) == 14:
             buffer_p2.popleft()
         buffer_p2.append(char)
 
 print("part1: ", p1_result)
-print("part1: ", p2_result)
+print("part2: ", p2_result)
