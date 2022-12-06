012e7e2b092799f49e6188035e8c5173f6099348
Marcin Gomulka <magomulka@gmail.com>
2022-12-06 06:32:21 +0100

    solution for day06

---
+++ day06/main.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+lines = []
+for line in sys.stdin:
+    lines.append(line)
+
+char_list = [*lines[0]]
+buffer_p1 = deque()
+buffer_p2 = deque()
+
+p1_result = 0
+p2_result = 0
+for i in range(len(char_list)):
+    char = char_list[i]
+    if p1_result == 0 and len(buffer_p1) == 4 and len(set(buffer_p1)) == 4:
+        p1_result = i
+    else:
+        if len(buffer_p1) == 4:
+            buffer_p1.popleft()
+        buffer_p1.append(char)
+
+    if p2_result == 0 and (len(buffer_p2)) == 14 and len(set(buffer_p2)) == 14:
+        p2_result = i
+    else:
+        if len(buffer_p2) == 14:
+            buffer_p2.popleft()
+        buffer_p2.append(char)
+
+print("part1: ", p1_result)
+print("part1: ", p2_result)
