0f3b2b7329ad99ea3644a1edfb50820baa640ce4
Marcin Gomulka <magomulka@gmail.com>
2022-12-10 07:35:55 +0100

    solution for day10 - refactor

--- day10/main.py
+++ day10/main.py
@@ -6,45 +6,46 @@ THRESHOLDS = {40, 80, 120, 160, 200, 240}
 
 
 class Instruction:
-    def __init__(self, instruction, value):
-        self.instruction = instruction
+    def __init__(self, command, value):
+        self.command = command
         self.value = value
 
     def __repr__(self):
-        return "(" + self.instruction + "): " + str(self.value)
+        return "(" + self.command + "): " + str(self.value)
 
 
 lines = []
 for line in sys.stdin:
     lines.append(line.strip())
-instructions = []
+instr_list = []
 for line in lines:
     chunks = line.split()
     if len(chunks) == 2:
-        instructions.append(Instruction(chunks[0], int(chunks[1])))
+        instr_list.append(Instruction(chunks[0], int(chunks[1])))
     else:
-        instructions.append((Instruction(chunks[0], 0)))
-cycles = 1
-register_x = 1
-p1_result = 0
-row = 0
-col = 0
+        instr_list.append(Instruction(chunks[0], 0))
+
 screen = []
 for i in range(6):
     screen.append(["."] * 40)
-for instr in instructions:
-    for i in range(CYCLES[instr.instruction]):
-        if col in {register_x, register_x + 1, register_x - 1}:
+cycle = 1
+x = 1
+p1_result = 0
+row = 0
+col = 0
+for instr in instr_list:
+    for i in range(CYCLES[instr.command]):
+        if col in {x, x + 1, x - 1}:
             screen[row][col] = "#"
         col += 1
-        if cycles in STEPS:
-            p1_result += cycles * register_x
-        if cycles in THRESHOLDS:
+        if cycle in STEPS:
+            p1_result += cycle * x
+        if cycle in THRESHOLDS:
             row += 1
             col = 0
-        cycles += 1
-    if instr.instruction == "addx":
-        register_x += instr.value
+        cycle += 1
+    if instr.command == "addx":
+        x += instr.value
 print("part1:", p1_result)
 print("part2:")
 for r in screen:
