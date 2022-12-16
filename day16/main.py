95382eda89bf391685ed0ebe4e097dec209dfbf2
Marcin Gomulka <magomulka@gmail.com>
2022-12-16 21:41:35 +0100

    solution for day16 - part1 performance improvement

--- day16/main.py
+++ day16/main.py
@@ -72,57 +72,48 @@ def undo_leak(valves, count):
 
 
 def try_solve(timer, valves, current, cache):
-    global total_leak_value, leaking_valves, open_valves
+    global total_leak_value, positive_flow_valves, open_valves
     key = Branch(timer, current.name, total_leak_value, valves)
     if key in cache.keys():
         return cache[key]
     if timer == 30:
         return total_leak_value
     max_total_leak = 0
-    for i in range(2):
-        # decision open valve or go to neighbor or wait if all open
-        if i == 0 and current.flow > 0 and not current.open:
-            # open valve
-            do_leak(valves, 1)
-            current.open = True
-            open_valves += 1
-            leak = try_solve(timer + 1, valves, current, cache)
-            if leak > max_total_leak:
-                max_total_leak = leak
-            current.open = False
-            open_valves -= 1
-            undo_leak(valves, 1)
-        elif i == 1 and open_valves != leaking_valves:
-            # go to neighbor
-            for neighbor_name in current.neighbors.keys():
-                neighbor = valves[neighbor_name]
-                neighbor_weight = current.neighbors[neighbor_name]
-                if timer + neighbor_weight <= 30:
-                    do_leak(valves, neighbor_weight)
-                    leak = try_solve(timer + neighbor_weight, valves, neighbor, cache)
-                    if leak > max_total_leak:
-                        max_total_leak = leak
-                    undo_leak(valves, neighbor_weight)
-        elif i == 1 and open_valves == leaking_valves:
-            # wait remaining time
-            do_leak(valves, 1)
-            leak = try_solve(timer + 1, valves, current, cache)
-            if leak > max_total_leak:
-                max_total_leak = leak
-            undo_leak(valves, 1)
+    if open_valves == positive_flow_valves:
+        # wait remaining time
+        do_leak(valves, 30 - timer)
+        max_total_leak = total_leak_value
+        undo_leak(valves, 30 - timer)
+    else:
+       for neighbor_name in current.neighbors.keys():
+            # go to neighbor and open it
+            neighbor = valves[neighbor_name]
+            if neighbor.flow == 0:
+                continue
+            neighbor_weight = current.neighbors[neighbor_name]
+            if not neighbor.open and timer + neighbor_weight + 1 <= 30:
+                do_leak(valves, neighbor_weight + 1)
+                neighbor.open = True
+                open_valves += 1
+                leak = try_solve(timer + neighbor_weight + 1, valves, neighbor, cache)
+                if leak > max_total_leak:
+                    max_total_leak = leak
+                neighbor.open = False
+                open_valves -= 1
+                undo_leak(valves, neighbor_weight + 1)
     cache[key] = max_total_leak
     return max_total_leak
 
 
 total_leak_value = 0
 open_valves = 0
-leaking_valves = 0
+positive_flow_valves = 0
 lines = []
 for line in sys.stdin:
     lines.append(line.strip())
 
-valves = dict()
-reduced_valves = dict()
+valve_graph = dict()
+reduced_valve_graph = dict()
 for line in lines:
     parts = line.split(";")
     chunks1 = parts[0].split()
@@ -133,24 +124,22 @@ for line in lines:
     for neighbor in chunks2:
         neighbor_dists[neighbor.strip()] = 1
     if flow > 0:
-        reduced_valves[name] = Valve(name, flow, dict())
-        leaking_valves += 1
-    valves[name] = Valve(name, flow, neighbor_dists)
+        reduced_valve_graph[name] = Valve(name, flow, dict())
+        positive_flow_valves += 1
+    valve_graph[name] = Valve(name, flow, neighbor_dists)
 
-start = valves["AA"]
-reduced_valves["AA"] = Valve(start.name, start.flow, dict())
-for reduced_valve1 in reduced_valves.values():
-    for reduced_valve2 in reduced_valves.values():
+start = valve_graph["AA"]
+reduced_valve_graph["AA"] = Valve(start.name, start.flow, dict())
+for reduced_valve1 in reduced_valve_graph.values():
+    for reduced_valve2 in reduced_valve_graph.values():
         if reduced_valve1 == reduced_valve2:
             continue
-        dist = bfs(valves[reduced_valve1.name], valves[reduced_valve2.name], valves)
+        dist = bfs(valve_graph[reduced_valve1.name], valve_graph[reduced_valve2.name], valve_graph)
         reduced_valve1.neighbors[reduced_valve2.name] = dist
         reduced_valve2.neighbors[reduced_valve1.name] = dist
 
-valves_copy = deepcopy(reduced_valves)
-current = reduced_valves["AA"]
+# valves_copy = deepcopy(reduced_valve_graph)
 start_time = time.time()
-total_leak = try_solve(0, reduced_valves, current, dict())
-
-print("part1:", total_leak)
+max_leak = try_solve(0, reduced_valve_graph, reduced_valve_graph["AA"], dict())
+print("part1:", max_leak)
 print("elapsed time [s]:", time.time() - start_time)
